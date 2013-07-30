f9483499785014471278055179b2986df9023ea6
Raymond Feng <raymond@strongloop.com>
2013-07-30 14:21:10 -0700

    Rename 'loopback-data' to 'loopback-datasource-juggler'

--- example/app.js
+++ example/app.js
@@ -1,4 +1,4 @@
-var DataSource = require('loopback-data').DataSource;
+var DataSource = require('loopback-datasource-juggler').DataSource;
 
 var ds = new DataSource(require('../'), {
     host: '166.78.158.45',
