a004b03d0f1dd0166888776b1aa96ceb339219a1
Simon Ho <simonho@ca.ibm.com>
2016-08-18 11:24:31 -0700

    Fix pretest and init test configs
    Environment variables set in pretest.js and test/init.js were silently
    being converted to Strings. For example, when TEST_MYSQL_USER is
    undefined, it's value was automatically converted to the String
    'undefined'. This is documented a side effect of assigning values to
    process.env. See https://nodejs.org/api/process.html#process_process_env
    for more details

--- pretest.js
+++ pretest.js
@@ -1,15 +1,19 @@
 'use strict';
-// TODO: used for testing support for parallel testing on ci.strongloop.com which
-// provides MYSQL_* env vars instead of TEST_MYSQL_* env vars.
-process.env.TEST_MYSQL_USER = process.env.TEST_MYSQL_USER || process.env.MYSQL_USER;
-process.env.TEST_MYSQL_PASSWORD = process.env.TEST_MYSQL_PASSWORD || process.env.MYSQL_PASSWORD;
-process.env.TEST_MYSQL_HOST = process.env.TEST_MYSQL_HOST || process.env.MYSQL_HOST;
-process.env.TEST_MYSQL_PORT = process.env.TEST_MYSQL_PORT || process.env.MYSQL_PORT;
 
-if (!process.env.TEST_MYSQL_USER) {
-  console.log('not seeding DB with example db');
-  return;
+if (!process.env.TEST_MYSQL_USER &&
+    !process.env.MYSQL_USER &&
+    !process.env.CI) {
+  return console.log('Not seeding DB with test db');
 }
+
+process.env.TEST_MYSQL_HOST =
+  process.env.TEST_MYSQL_HOST || process.env.MYSQL_HOST || 'localhost';
+process.env.TEST_MYSQL_PORT =
+  process.env.TEST_MYSQL_PORT || process.env.MYSQL_PORT || 3306;
+process.env.TEST_MYSQL_USER =
+  process.env.TEST_MYSQL_USER || process.env.MYSQL_USER || 'test';
+process.env.TEST_MYSQL_PASSWORD =
+  process.env.TEST_MYSQL_PASSWORD || process.env.MYSQL_PASSWORD || 'test';
 
 var fs = require('fs');
 var cp = require('child_process');
