603d8b5b20eeaa3bb4aecfc945615f38af24b64a
ssh24 <sshasan10@hotmail.com>
2017-05-11 19:42:09 -0400

    Add docker setup
    Add a setup script that spawns a docker container before running test
    Easy for contributors now to test, no need for a local instance

---
+++ setup.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+### Shell script to spin up a docker container for mysql.
+
+## color codes
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+PLAIN='\033[0m'
+
+## variables
+MYSQL_CONTAINER="mysql_c"
+USER="root"
+PASSWORD="pass"
+PORT=3306
+
+## check if docker exists
+printf "\n${RED}>> Checking for docker${PLAIN} ${GREEN}...${PLAIN}"
+docker -v > /dev/null 2>&1
+DOCKER_EXISTS=$?
+if [ "$DOCKER_EXISTS" -ne 0 ]; then
+    printf "\n${CYAN}Status: ${PLAIN}${RED}Docker not found. Terminating setup.${PLAIN}\n"
+    exit 1
+fi
+printf "\n${CYAN}Found docker. Moving on with the setup.${PLAIN}\n"
+
+
+## cleaning up previous builds
+printf "\n${RED}>> Finding old builds and cleaning up${PLAIN} ${GREEN}...${PLAIN}"
+docker rm -f $MYSQL_CONTAINER > /dev/null 2>&1
+printf "\n${CYAN}Clean up complete.${PLAIN}\n"
+
+## pull latest mysql image
+printf "\n${RED}>> Pulling latest mysql image${PLAIN} ${GREEN}...${PLAIN}"
+docker pull mysql:latest > /dev/null 2>&1
+printf "\n${CYAN}Image successfully built.${PLAIN}\n"
+
+## run the mysql container
+printf "\n${RED}>> Starting the mysql container${PLAIN} ${GREEN}...${PLAIN}"
+docker run --name $MYSQL_CONTAINER -e MYSQL_ROOT_USER=$USER -e MYSQL_ROOT_PASSWORD=$PASSWORD -p $PORT:3306 -d mysql:latest > /dev/null 2>&1
+docker cp ./test/schema.sql $MYSQL_CONTAINER:/home/ > /dev/null 2>&1
+printf "\n${CYAN}Container is up and running.${PLAIN}\n"
+
+## export the schema to the mysql database
+printf "\n${RED}>> Exporting schema to database${PLAIN} ${GREEN}...${PLAIN}\n"
+
+## command to export schema
+docker exec -it $MYSQL_CONTAINER /bin/sh -c "mysql -uroot -ppass < /home/schema.sql" > /dev/null 2>&1
+
+## variables needed to health check export schema
+OUTPUT=$?
+TIMEOUT=120
+TIME_PASSED=0
+WAIT_STRING="."
+
+printf "\n${GREEN}Waiting for database to respond with updated schema $WAIT_STRING${PLAIN}"
+while [ "$OUTPUT" -ne 0 ] && [ "$TIMEOUT" -gt 0 ]
+    do
+        docker exec -it $MYSQL_CONTAINER /bin/sh -c "mysql -uroot -ppass < /home/schema.sql" > /dev/null 2>&1
+        OUTPUT=$?
+        sleep 1s
+        let "TIMEOUT = $TIMEOUT - 1"
+        let "TIME_PASSED = $TIME_PASSED + 1"
+
+        if [ "$TIME_PASSED" -eq 5 ]; then
+            printf "${GREEN}.${PLAIN}"
+            TIME_PASSED=0
+        fi
+    done
+
+if [ "$TIMEOUT" -le 0 ]; then
+    printf "\n\n${CYAN}Status: ${PLAIN}${RED}Failed to export schema. Terminating setup.${PLAIN}\n"
+    exit 1
+fi
+printf "\n${CYAN}Successfully exported schema to database.${PLAIN}\n"
+
+## set env variables for running test
+printf "\n${RED}>> Setting env variables to run test${PLAIN} ${GREEN}...${PLAIN}"
+export MYSQL_HOST=0.0.0.0
+export MYSQL_PORT=$PORT
+export MYSQL_USER=$USER
+export MYSQL_PASSWORD=$PASSWORD
+printf "\n${CYAN}Env variables set.${PLAIN}\n"
+
+printf "\n${CYAN}Status: ${PLAIN}${GREEN}Set up completed successfully.${PLAIN}\n"
+printf "\n${CYAN}To run the test suite:${PLAIN} ${YELLOW}npm test${PLAIN}\n\n"
