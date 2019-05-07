a12558e8a43acde4c72af3257ba55d71d0c9d80d
Agnes Lin <agneslin.lin@ibm.com>
2019-05-07 13:25:30 -0400

    chore: update copyrights years

--- pretest.js
+++ pretest.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2016,2017. All Rights Reserved.
+// Node module: loopback-connector-mysql
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 'use strict';
 
 if (!process.env.CI) {
