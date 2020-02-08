2bb2f8d51f336cddc1f918f35b0f719dcedc508b
Diana Lau <dhmlau@ca.ibm.com>
2020-02-08 17:23:33 -0500

    chore: update copyrights year

--- deps/juggler-v3/test.js
+++ deps/juggler-v3/test.js
@@ -1,7 +1,7 @@
 // Copyright IBM Corp. 2019. All Rights Reserved.
 // Node module: loopback-connector-mysql
-// This file is licensed under the Artistic License 2.0.
-// License text available at https://opensource.org/licenses/Artistic-2.0
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
 
 'use strict';
 
